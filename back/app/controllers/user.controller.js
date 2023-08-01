5c4663fcd624efc744a1f7ab6b91eace8be35b96
jumo04 <juanfernandom967@gmail.com>
2023-08-01 16:56:41 -0500

    first part of the login

---
+++ back/app/controllers/user.controller.js
@@ -0,0 +1,15 @@
+exports.allAccess = (req, res) => {
+    res.status(200).send("Public Content.");
+  };
+  
+  exports.testigoBoard = (req, res) => {
+    res.status(200).send("Testigo Content.");
+  };
+  
+  exports.adminBoard = (req, res) => {
+    res.status(200).send("Admin Content.");
+  };
+  
+  exports.candidatoBoard = (req, res) => {
+    res.status(200).send("Candidato Content.");
+  };
--- back/server.js
+++ back/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const db = require("./app/models");
+
+const dbConfig = require("./app/config/db.config");
+
+const Role = db.role;
 
 var corsOptions = {
   origin: "http://localhost:8081"
@@ -15,10 +20,68 @@ app.use(express.json());
 // express parse - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-// aca se colocan las rutas
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+
+  function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+      if (!err && count === 0) {
+        new Role({
+          name: "admin"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'admin' to roles collection");
+        });
+  
+        new Role({
+          name: "candidato"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'candidato' to roles collection");
+        });
+  
+        new Role({
+          name: "testigo"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'testigo' to roles collection");
+        });
+      }
+    });
+  }
+
+
+//Aca se colocan las rutas
 app.get("/", (req, res) => {
   res.json({ message: "Bienvenido a mi apliacion para conteo de votos a traves de los formularios." });
 });
+
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
 
 // puerto
 const PORT = process.env.PORT || 8080;
